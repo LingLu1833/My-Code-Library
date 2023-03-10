a65da41fb59ff731da7e646bb39f3f7b9c64d7d8
LingLu <71413848+LingLu1833@users.noreply.github.com>
2023-03-10 12:33:59 +0800

    DLL注入和卸载

---
+++ Injector/injector.cpp
@@ -0,0 +1,107 @@
+#include "injector.h"
+
+Injector::Injector(DWORD pid, LPCWSTR dllPath)
+{
+	m_pid = pid;
+	m_dllPath = dllPath;
+}
+
+Injector::~Injector()
+{
+}
+
+bool Injector::Inject()
+{
+	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, m_pid);
+	if (hProcess == NULL)
+	{
+		return false;
+	}
+
+	LPVOID pRemoteMem = VirtualAllocEx(hProcess, NULL, wcslen(m_dllPath) + 1, MEM_COMMIT, PAGE_READWRITE);
+	if (pRemoteMem == NULL)
+	{
+		CloseHandle(hProcess);
+		return false;
+	}
+
+	if (!WriteProcessMemory(hProcess, pRemoteMem, m_dllPath, wcslen(m_dllPath) + 1, NULL)) {
+		VirtualFreeEx(hProcess, pRemoteMem, 0, MEM_RELEASE);
+		CloseHandle(hProcess);
+		return false;
+	}
+
+	HMODULE hKernel32 = GetModuleHandle(L"Kernel32");
+	if (hKernel32 == NULL)
+	{
+		VirtualFreeEx(hProcess, pRemoteMem, 0, MEM_RELEASE);
+		CloseHandle(hProcess);
+		return false;
+	}
+
+	FARPROC pLoadLibrary = GetProcAddress(hKernel32, "LoadLibraryW");
+	if (pLoadLibrary == NULL)
+	{
+		VirtualFreeEx(hProcess, pRemoteMem, 0, MEM_RELEASE);
+		CloseHandle(hProcess);
+		return false;
+	}
+
+	HANDLE hThread = CreateRemoteThread(hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)pLoadLibrary, pRemoteMem, 0, NULL);
+	if (hThread == NULL)
+	{
+		VirtualFreeEx(hProcess, pRemoteMem, 0, MEM_RELEASE);
+		CloseHandle(hProcess);
+		return false;
+	}
+
+	WaitForSingleObject(hThread, INFINITE);
+	VirtualFreeEx(hProcess, pRemoteMem, 0, MEM_RELEASE);
+	CloseHandle(hThread);
+	CloseHandle(hProcess);
+
+	return true;
+}
+
+bool Injector::Unload()
+{
+	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, m_pid);
+	if (hProcess == NULL)
+	{
+		return false;
+	}
+
+	HMODULE hKernel32 = GetModuleHandle(L"Kernel32");
+	if (hKernel32 == NULL)
+	{
+		CloseHandle(hProcess);
+		return false;
+	}
+
+	FARPROC pFreeLibrary = GetProcAddress(hKernel32, "FreeLibrary");
+	if (pFreeLibrary == NULL)
+	{
+		CloseHandle(hProcess);
+		return false;
+	}
+
+	HMODULE hModule = GetModuleHandle(m_dllPath);
+	if (hModule == NULL)
+	{
+		CloseHandle(hProcess);
+		return false;
+	}
+
+	HANDLE hThread = CreateRemoteThread(hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)pFreeLibrary, (LPVOID)hModule, 0, NULL);
+	if (hThread == NULL)
+	{
+		CloseHandle(hProcess);
+		return false;
+	}
+
+	WaitForSingleObject(hThread, INFINITE);
+	CloseHandle(hThread);
+	CloseHandle(hProcess);
+
+	return true;
+}
---
+++ Injector/injector.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <Windows.h>
+
+class Injector
+{
+public:
+	Injector(DWORD pid, LPCWSTR dllPath);
+	~Injector();
+
+	bool Inject();
+
+	bool Unload();
+
+private:
+	DWORD m_pid;
+	LPCWSTR m_dllPath;
+};
